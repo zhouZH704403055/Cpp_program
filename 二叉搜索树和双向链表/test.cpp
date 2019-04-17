e3fc1b6cace96272c819dfcfe82480e52cfc4a33
zhouZH704403055 <704403055hao@sina.com>
2019-04-17 22:58:08 +0800

    将二叉搜索树转换成有序的双向链表

---
+++ 二叉搜索树和双向链表/test.cpp
@@ -0,0 +1,49 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    TreeNode* Convert(TreeNode* pRootOfTree)
+    {
+        if(pRootOfTree==nullptr)
+        {
+            return nullptr;
+        }
+        TreeNode* NewList=nullptr;
+        Convert(pRootOfTree,&NewList);
+        TreeNode* Head=NewList;
+        while(Head&&Head->left)
+        {
+            Head=Head->left;
+        }
+        return Head;
+    }
+    void Convert(TreeNode* pRootOfTree,TreeNode** NewList)
+    {
+        TreeNode* cur=pRootOfTree;
+        if(pRootOfTree==nullptr)
+        {
+            return;
+        }
+        if(cur->left)
+        {
+            Convert(cur->left,NewList);
+        }
+        cur->left=*NewList;
+        if(*NewList!=nullptr)
+        {
+            (*NewList)->right=cur;
+        }
+        *NewList=cur;
+        if(cur->right)
+        {
+            Convert(cur->right,NewList);
+        }
+    }
+};
