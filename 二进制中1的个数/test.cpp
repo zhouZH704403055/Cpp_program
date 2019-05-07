2aa5573de4327007d09ae794a7849f5da5b95182
zhouZH704403055 <704403055hao@sina.com>
2019-05-07 22:08:13 +0800

    计算一个十进制整数的二进制中有多少个1

---
+++ 二进制中1的个数/test.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+     int  NumberOf1(int n) {
+         int count=0;
+         while(n)
+         {
+             n&=(n-1);
+             count++;
+         }
+         return count;
+     }
+};
