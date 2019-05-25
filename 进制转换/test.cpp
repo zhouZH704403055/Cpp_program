6aa8eb7065b53916f27203355770fec5662f9b82
zhouZH704403055 <704403055hao@sina.com>
2019-05-25 22:03:31 +0800

    将十进制数m转换成任意n进制数，n最大为16

---
+++ 进制转换/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+string s, table="0123456789ABCDEF";
+int m,n;
+cin>>m>>n;
+bool flag = false;
+// 如果是负数，则转成正数，并标记一下
+if(m < 0)
+{
+m = 0 - m;
+flag = true;
+} /
+/ 按进制换算成对应的字符添加到s
+while(m)
+{
+s += table[m%n];
+m /= n;
+} i
+f(flag)
+s += '-';
+reverse(s.begin(), s.end());
+cout<<s<<endl;
+return 0;
+}
