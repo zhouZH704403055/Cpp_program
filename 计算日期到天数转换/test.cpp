df7091936bfc99c0207daf0d0c2bfb7506bdd39e
zhouZH704403055 <704403055hao@sina.com>
2019-06-11 22:32:37 +0800

    根据输入的日期，计算是这一年的第几天

---
+++ 计算日期到天数转换/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+	int array[12] = {31,59, 90, 120, 151, 181, 212,243, 273, 304, 334, 365};
+	int year;
+	int month;
+	int day;
+	int sum = 0;
+	while(cin >> year >> month >> day)
+	{ 
+		sum = 0;
+		sum += array[month - 2];
+		sum += day;
+	if(month > 2)
+	{ 
+		if((year % 4 == 0 && year % 100 != 0)|| year % 400 == 0)
+		{
+			sum += 1;
+		} 
+	} 
+	cout << sum <<endl;
+	} 
+}
