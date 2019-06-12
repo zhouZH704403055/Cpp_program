77ecc9e06c9cb535fd0a2a1a6f3310ed955ba6cb
zhouZH704403055 <704403055hao@sina.com>
2019-06-12 22:47:03 +0800

    有n种颜色的手套，手套分左右手，现在只能分辨出左右手，至少一次要拿多少个手套才能拿保证一定能选出一双颜色相同的手套

---
+++ 手套/test.cpp
@@ -0,0 +1,23 @@
+class Gloves
+{ public:int findMinimum(int n, vector<int> left, vector<int> right) {
+int left_sum = 0, left_min = INT_MAX;
+int right_sum = 0, right_min = INT_MAX;
+int sum = 0;
+//遍历每一种颜色的左右手套序列
+for(int i=0;i<n;i++){
+//对于有0存在的颜色手套，累加
+if(left[i]*right[i]==0)
+sum += left[i] + right[i];
+//对于左右手都有的颜色手套，执行累加-最小值+1
+//找到最小值和总数
+else{
+left_sum += left[i];
+right_sum += right[i];
+left_min = min(left_min, left[i]);
+right_min = min(right_min, right[i]);
+}
+} /
+/结果为有左右都有数量的手套序列的结果+有0存在的手套数+最后再加一肯定就能保证了
+return sum + min(left_sum-left_min+1, right_sum-right_min+1) + 1;
+}
+};
