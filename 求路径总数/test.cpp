0c2ab2ea4e4d65c202f16e79701adc263e028cf0
zhouZH704403055 <704403055hao@sina.com>
2019-07-08 23:28:48 +0800

    updata code..

--- 求路径总数/test.cpp
+++ 求路径总数/test.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 int pathNum(int n,int m)
 {
-if(n > 1 && m > 1)
-//b情况，递归
-return pathNum(n-1,m) + pathNum(n,m-1);
-else if(((n >= 1)&&(m == 1))||((n == 1)&&(m >= 1)))
-// a情况，终止条件
-return n + m;
-else
-//格子为0时， 路径为0
-return 0;
-} i
-nt main()
+	if(n > 1 && m > 1)
+		//b情况，递归
+		return pathNum(n-1,m) + pathNum(n,m-1);
+	else if(((n >= 1)&&(m == 1))||((n == 1)&&(m >= 1)))
+	// a情况，终止条件
+		return n + m;
+	else
+	//格子为0时， 路径为0
+	return 0;
+}
+ 
+int main()
 {
-int n,m;
-while(cin>>n>>m)
-{
-cout<<pathNum(n,m)<<endl;
-} r
-eturn 0;
+	int n,m;
+	while(cin>>n>>m)
+	{
+		cout<<pathNum(n,m)<<endl;
+	} 
+	return 0;
 }
