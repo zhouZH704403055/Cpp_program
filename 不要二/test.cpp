6f0d18bb035260f4a2916a3707626aaaed896743
zhouZH704403055 <704403055hao@sina.com>
2019-07-10 22:53:48 +0800

    update code...

--- 不要二/test.cpp
+++ 不要二/test.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 int main()
 {
-int w,h,res = 0;
-cin >> w >> h;
-vector<vector<int>> a;
-a.resize(w);
-for(auto& e : a)
-e.resize(h, 1);
-for(int i=0;i<w;i++)
-{
-for(int j=0;j<h;j++)
-{
-if(a[i][j]==1)
-{
-res++;
-// 标记不能放蛋糕的位置
-if((i+2)<w)
-a[i+2][j] = 0;
-if((j+2)<h)
-a[i][j+2] = 0;
-}
-}
-} c
-out << res;
-return 0;
+	int w,h,res = 0;
+	cin >> w >> h;
+	vector<vector<int>> a;
+	a.resize(w);
+	for(auto& e : a)
+		e.resize(h, 1);
+	for(int i=0;i<w;i++)
+	{
+		for(int j=0;j<h;j++)
+		{
+			if(a[i][j]==1)
+			{
+				res++;
+				// 标记不能放蛋糕的位置
+				if((i+2)<w)
+					a[i+2][j] = 0;
+				if((j+2)<h)
+					a[i][j+2] = 0;
+			}
+		}
+	} 
+	cout << res <<endl;
+	return 0;
 }
