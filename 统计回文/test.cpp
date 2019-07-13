9c238a675128501130f5d573a5eb20ca9f50764d
zhouZH704403055 <704403055hao@sina.com>
2019-07-13 21:54:28 +0800

    update code..

--- 统计回文/test.cpp
+++ 统计回文/test.cpp
@@ -1,34 +1,37 @@
 #include<iostream>
 #include<string>
+
 using namespace std;
+
 // 判断是否是回文
 bool IsCircleText(const string& s)
 {
-size_t begin = 0;
-size_t end = s.size()-1;
-while(begin < end)
+	size_t begin = 0;
+	size_t end = s.size()-1;
+	while(begin < end)
+	{
+		if(s[begin] != s[end])
+			return false;
+		++begin;
+		--end;
+	} 
+	return true;
+}
+ 
+int main()
 {
-if(s[begin] != s[end])
-return false;
-++begin;
---end;
-} r
-eturn true;
-} i
-nt main()
-{
-std::string str1, str2;
-getline(cin, str1);
-getline(cin, str2);
-size_t count = 0;
-for(size_t i = 0; i <= str1.size(); ++i)
-{
-// 将字符串2插入到字符串1的每个位置，再判断是否是回文
-string str = str1;
-str.insert(i, str2);
-if(IsCircleText(str))
-++count;
-} c
-out<<count<<endl;
-return 0;
+	std::string str1, str2;
+	getline(cin, str1);
+	getline(cin, str2);
+	size_t count = 0;
+	for(size_t i = 0; i <= str1.size(); ++i)
+	{
+		// 将字符串2插入到字符串1的每个位置，再判断是否是回文
+		string str = str1;
+		str.insert(i, str2);
+		if(IsCircleText(str))
+			++count;
+	} 
+	cout<<count<<endl;
+	return 0;
 }
