fb56704d4615a5abca25fee74b36449cee248a03
zhouZH704403055 <704403055hao@sina.com>
2019-07-19 22:12:53 +0800

    update code..

--- 栈的压入弹出序列/test.cpp
+++ 栈的压入弹出序列/test.cpp
@@ -1,6 +1,8 @@
+
 class Solution {
 public:
-    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) 
+	{
         stack<int> stk;
         int j=0;
         for(int i=0;i<pushV.size();i++)
