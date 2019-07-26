39b486298e3fdc67aa7c7335295efb4b4f81c438
zhouZH704403055 <704403055hao@sina.com>
2019-07-26 23:53:04 +0800

    update code..

--- 星际密码/test.cpp
+++ 星际密码/test.cpp
@@ -3,21 +3,22 @@
 std::vector<int> a = {1,1};
 void data_init()
 {
-int i;
-for(i=2;i<10005;i++){
-a.push_back((a[i-1]+a[i-2]) % 10000);
+	int i;
+	for(i=2;i<10005;i++){
+		a.push_back((a[i-1]+a[i-2]) % 10000);
+	}
 }
-} i
-nt main()
+ 
+int main()
 {
-int n,t;
-data_init();
-while(std::cin >> n){
-while(n--){
-std::cin >> t;
-printf("%04d",a[t])
-} p
-rintf("\n");
-} r
-eturn 0;
+	int n,t;
+	data_init();
+	while(std::cin >> n){
+		while(n--){
+			std::cin >> t;
+			printf("%04d",a[t])
+		}	 
+		printf("\n");
+	} 
+	return 0;
 }
