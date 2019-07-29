07027d74a928713f52761b180ce78f4886efe0cc
zhouZH704403055 <704403055hao@sina.com>
2019-07-29 20:05:54 +0800

    update code..

---
+++ 快到碗里来/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+double n,r;
+while(cin >> n >> r){
+//周长：2*r*3.1415
+//身长：n
+if(n > (2*r*3.1415))
+cout<<"No"<<endl;
+elsecout<<"Yes"<<endl;
+} r
+eturn 0;
+}
