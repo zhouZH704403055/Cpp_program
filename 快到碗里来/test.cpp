0e1f56d9b3215784f053b855ef988d74cfd96ee8
zhouZH704403055 <704403055hao@sina.com>
2019-07-29 20:07:31 +0800

    update code..

--- 快到碗里来/test.cpp
+++ 快到碗里来/test.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+
 using namespace std;
-int main(){
-double n,r;
-while(cin >> n >> r){
-//周长：2*r*3.1415
-//身长：n
-if(n > (2*r*3.1415))
-cout<<"No"<<endl;
-elsecout<<"Yes"<<endl;
-} r
-eturn 0;
+
+int main()
+{
+	double n,r;
+	while(cin >> n >> r)
+	{
+		//周长：2*r*3.1415
+		//身长：n
+		if(n > (2*r*3.1415))
+			cout<<"No"<<endl;
+		else 
+			cout<<"Yes"<<endl;
+	} 
+	return 0;
 }
