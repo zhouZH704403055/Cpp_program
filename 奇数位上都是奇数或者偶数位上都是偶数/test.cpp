89dc05595b020d3dfdf17a75fa84beec46d6a51e
zhouZH704403055 <704403055hao@sina.com>
2019-08-07 20:02:39 +0800

    update code..

---
+++ 奇数位上都是奇数或者偶数位上都是偶数/test.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+void oddInOddEvenInEven(std::vector<int>& arr, int len) {
+long i = 0, j = 1;
+while(i < len && j < len){
+if((arr[i] % 2) == 0) { i += 2; continue; }//偶数位上寻找非偶数
+if((arr[j] % 2) != 0) { j += 2; continue; }//奇数位上寻找非奇数
+swap(arr[i], arr[j]);
+}
+}
+};
