d08a886c763937bd80f792fe7a05bfb1a3cc1ba9
zhouZH704403055 <704403055hao@sina.com>
2019-08-10 18:50:44 +0800

    update code..

---
+++ 有假币/test.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(){
+long long n;int cnt;
+while ((scanf("%lld",&n)) != EOF) {
+if (n == 0)
+break;
+cnt=0;
+while (n >= 2) {
+if (n % 3) {
+//不可以整除则取最差情况：最重的一份是 n/3 + 1个金币
+n=n/3+1;
+}else {
+//可以整除在直接整除，能够获取到最重的一份
+n/=3;
+} c
+nt++;
+}printf("%d\n",cnt);
+};
+return 0;
+}
