2c014490a4c92f117588aa8798b8426e271509ca
zhouZH704403055 <704403055hao@sina.com>
2019-08-14 23:49:03 +0800

    update code..

--- 最难的问题/test.cpp
+++ 最难的问题/test.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main() {
-char c;
-while ((c = getchar()) != EOF){
-if ('A' <= c && 'Z' >= c) {
-c = (c > 'E') ? (c - 5) : (c + 21);
-} p
-utchar(c);
-} r
-eturn 0;
+	char c;
+	while ((c = getchar()) != EOF){
+		if ('A' <= c && 'Z' >= c) {
+			c = (c > 'E') ? (c - 5) : (c + 21);
+			} 
+			putchar(c);
+		} 
+	return 0;
 }
