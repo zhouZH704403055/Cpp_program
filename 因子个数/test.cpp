cfff6db10a5b8c96947b28e575814d3c32f7065e
zhouZH704403055 <704403055hao@sina.com>
2019-08-14 23:47:42 +0800

    update code..

--- 因子个数/test.cpp
+++ 因子个数/test.cpp
@@ -1,22 +1,24 @@
-#
-include <iostream>
+#include <iostream>
 #include<math.h>
+
 using namespace std;
+
 int main()
-{int n,k,i;
-while(cin >> n){
-k=0;
-for(i = 2; i <= sqrt(n); i++) {
-if ((n % i) == 0) {
-while((n % i) == 0){
-n=n/i;
-} k
-++;
-}
-} i
-f(n!=1)
-k++;
-cout<<k<<endl;
-} r
-eturn 0;
+{
+	int n,k,i;
+	while(cin >> n){
+		k=0;
+		for(i = 2; i <= sqrt(n); i++) {
+			if ((n % i) == 0) {
+				while((n % i) == 0){
+					n=n/i;
+				} 
+				k++;
+			}
+		} 
+		if(n!=1)
+			k++;
+		cout<<k<<endl;
+	} 
+	return 0;
 }
