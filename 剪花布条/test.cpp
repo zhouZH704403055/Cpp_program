0c9a48762efdb1b2ba561768215464edbde9592f
zhouZH704403055 <704403055hao@sina.com>
2019-08-17 12:20:04 +0800

    update code..

--- 剪花布条/test.cpp
+++ 剪花布条/test.cpp
@@ -2,18 +2,18 @@
 #include<string>
 int main()
 {
-std::string s, t;
-while (std::cin >> s >> t)
-{
-int res = 0;
-size_t pos = 0;
-//依次在 s 中查找 t 即可，直到再也找不到 t
-while ((pos = s.find(t, pos)) != std::string::npos)
-{
-pos += t.size(); //跳过t串整体的长度
-++res; //计数
-} s
-td::cout << res << std::endl;
-} r
-eturn 0;
+	std::string s, t;
+	while (std::cin >> s >> t)
+	{
+		int res = 0;
+		size_t pos = 0;
+		//依次在 s 中查找 t 即可，直到再也找不到 t
+		while ((pos = s.find(t, pos)) != std::string::npos)
+		{
+			pos += t.size(); //跳过t串整体的长度
+			++res; //计数
+		}
+		std::cout << res << std::endl;
+	} 
+	return 0;
 }
