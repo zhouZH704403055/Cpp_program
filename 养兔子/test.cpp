7dee9e7983fad1d2fe40a51be8b0488de11af8ae
zhouZH704403055 <704403055hao@sina.com>
2019-08-18 21:53:30 +0800

    update code..

--- 养兔子/test.cpp
+++ 养兔子/test.cpp
@@ -1,16 +1,15 @@
-#
-include <iostream>
+#include <iostream>
 #include <cstdio>
 int main()
 {
-long long n[91] = { 1, 2 };
-for (int i = 2; i <= 90; i++)
-{
-n[i] = n[i - 1] + n[i - 2];
-} i
-nt d;
-while(std::cin >> d)
-{
-printf("%lld\n", n[d - 1]);
-}
+	long long n[91] = { 1, 2 };
+	for (int i = 2; i <= 90; i++)
+	{
+		n[i] = n[i - 1] + n[i - 2];
+	} 
+	int d;	
+	while(std::cin >> d)
+	{
+		printf("%lld\n", n[d - 1]);
+	}
 }
