f8c9c124cfb35302eb93ddf2cfc3c14f98f15a27
zhouZH704403055 <704403055hao@sina.com>
2019-08-19 22:08:37 +0800

    update code..

---
+++ 年会抽奖/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+int main()
+{
+long long d[22] = { 0, 0, 1 }; // 错排数量，预留第一项为0，配合下文中输入的n
+long long f[22] = { 0, 1, 2 }; // 阶 乘
+for (int i = 3; i < 22; i++)
+{
+d[i] = (i - 1) * (d[i - 1] + d[i - 2]); //错排的递推公式
+f[i] = i * f[i - 1]; //阶乘的递推公式
+} i
+nt n;
+while (std::cin >> n)
+{
+printf("%.2f%%\n", 100.0 * d[n] / f[n]); //用100.0来把结果处理成double，保留两位小数
+} r
+eturn 0;
+}
