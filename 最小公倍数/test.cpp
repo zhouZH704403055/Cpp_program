f1ea1a62bb8100021d69fb0ace313d0c3e9655bf
zhouZH704403055 <704403055hao@sina.com>
2019-09-14 23:58:06 +0800

    update code..

--- 最小公倍数/test.cpp
+++ 最小公倍数/test.cpp
@@ -1,20 +1,20 @@
-#
-include<iostream>
+#include<iostream>
 using namespace std;
 int gcd(int a, int b)
 {
-int r;
-while(r = a%b){
-a = b;
-b = r;
-} r
-eturn b;
-} i
-nt main()
+	int r;
+	while(r = a%b){
+		a = b;
+	b = r;
+	} 
+	return b;
+}
+
+int main()
 {
-int a,b;
-while(cin >> a >> b){
-cout << a*b/gcd(a,b) <<endl;
-} r
-eturn 0;
+	int a,b;
+	while(cin >> a >> b){
+		cout << a*b/gcd(a,b) <<endl;
+	} 
+	return 0;
 }
